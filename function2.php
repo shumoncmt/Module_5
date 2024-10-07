a6371f95bea8a5e7ba8493760ded2a9ce444e103
shumoncmt <shomoncmt@gmail.com>
2024-10-07 17:47:21 +0600

    add
    aa

---
+++ function2.php
@@ -0,0 +1,12 @@
+<?php
+function printStar($starCount){
+    for ($i = 0; $i < $starCount; $i++){
+        echo "*";
+    }
+    echo "\n";
+}
+printStar(10);
+printStar(20);
+printStar(30);
+printStar(40);
+printStar(50);
---
+++ function3.php
@@ -0,0 +1,8 @@
+<?php 
+function greet($name, $time ="morning"){
+    echo "Good $time $name \n";
+}
+greet("John");
+greet ("Shakil ");
+greet ("Doe", "evening");
+greet ("Jhon");
