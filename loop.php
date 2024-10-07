abdcaf3baeadbffe62550bdcf17081b05d560818
shumoncmt <shomoncmt@gmail.com>
2024-10-07 16:04:09 +0600

    add
    aa

--- loop.php
+++ loop.php
@@ -1,1 +1,20 @@
 <?php
+echo " This is heading \n";
+
+$hsaCount = 150;
+
+for ($i = 0; $i < $hsaCount; $i++) {
+    echo "#";
+}
+echo "\n";
+echo "This is Heading This  is Heading This is Heading This  is Heading This is Heading This  is HeadingThis is Heading This  is Heading  \n";
+for ($i = 0; $i < $hsaCount; $i++) {
+    echo "*";
+}
+
+echo "\n";
+echo "This is Heading This  is Heading This is Heading This  is Heading This is Heading This  is HeadingThis is Heading This  is Heading  \n";
+for ($i = 0; $i < $hsaCount; $i++) {
+    echo "#";
+}
+
