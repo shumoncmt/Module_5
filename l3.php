9c2cd9ac825cab2ba4d381cede4cc3ca9e585ffb
shumoncmt <shomoncmt@gmail.com>
2024-10-07 17:13:34 +0600

    add
    add

--- l3.php
+++ l3.php
@@ -1,7 +1,15 @@
 <?php
-$divisibleNumber = 500;
+$divisibleNumber = 25;
 for ($i = 0; $i < $divisibleNumber; $i++) {
-    if ($i % 3 ==1 || $i % 2 == 2){
+    if ($i % 3 ==1 || $i % 3 == 2){
         echo "{$i} is Not Divisible by 3 \n";
+    }
+}
+
+//  5 divisible Num list
+
+for ($i = 0; $i < $divisibleNumber; $i++) {
+    if ($i % 5 ==0 ){
+        echo "$i Divisible Number \n";
     }
 }
