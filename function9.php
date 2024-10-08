fb0984ccc5aec1e2e2afadfe8dbe9ae11958cd1e
shumoncmt <shomoncmt@gmail.com>
2024-10-08 14:42:46 +0600

    add

--- function9.php
+++ function9.php
@@ -21,3 +21,29 @@ argumentsPs("Abir", "52");
 argumentsPs("Shakil", "20");
 argumentsPs("Shumon", "66");
 argumentsPs("Rajib", "63");
+
+// PHP Call By Reference
+
+function adder (& $str2){
+    $str2 = "Call BY Reference";
+}
+$str = "Hello";
+adder($str);
+echo $str;
+echo "\n";
+//Default Argument Value
+
+function hello ($name= "Sonoo"){
+    echo "Hello $name \n";
+
+}
+hello ("Rajesh");
+hello();
+hello("shakil");
+echo "\n";
+
+//Returning Value
+function cube ($n){
+    return $n*$n*$n;
+}
+echo "Cube of 10 is " . cube(10);
