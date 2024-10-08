ec507fba8edfd8bbaab24ce2611af5be9f2a308c
shumoncmt <shomoncmt@gmail.com>
2024-10-08 12:46:18 +0600

    add
    ad

---
+++ function9.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// function allFunction()
+// {   
+//     function sayHello(){
+//         echo "Hello PHP Function ";
+    
+//     }
+//     sayHello();
+// }
+
+// allFunction();
+
+//PHP Arguments
+
+function argumentsPs($name){
+    echo "Hello $name \n";
+}
+argumentsPs("Abir");
+argumentsPs("Shakil");
+argumentsPs("Shumon");
+argumentsPs("Rajib");
