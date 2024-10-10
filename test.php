89d4b63f3db95bb5952bac8fbc9fbd6b2b4d9384
shumoncmt <shomoncmt@gmail.com>
2024-10-10 16:49:57 +0600

    add
    add

--- test.php
+++ test.php
@@ -1,4 +1,20 @@
 <?php
-$number = [15, 65, 80, 89, 30, 89, 658, 11110, 02];
-$largest = max($number);
-echo " The Largest Element is  $largest";
+// $number = [15, 65, 80, 89, 30, 89, 658, 11110, 02];
+// $largest = max($number);
+// echo " The Largest Element is  $largest";
+
+
+function Largest ($x , $y, $z){
+    $max = $x;
+    if ($x>= $y && $x <= $z)
+    $max = $x;
+ if ($y>= $y && $y <= $z)
+ $max = $y;
+
+if ($x>= $y && $x <= $z)
+$max = $x;
+echo "Largest  Number among $x, $y and $z is : $max \n";
+}
+Largest(100, 101, 25);
+Largest(520, 521, 262 );
+echo "\n";
